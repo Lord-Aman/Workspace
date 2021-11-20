529925ea011996d5d110a4799e72486145b336d0
Aman <aakashaman1770@gmail.com>
2021-11-20 17:36:51 +0000

    setup

--- OKL/oklwebapp/src/components/Navbar/Navbar.js
+++ OKL/oklwebapp/src/components/Navbar/Navbar.js
@@ -1,57 +1,21 @@
-import React, { Component } from "react";
-import Logo from "../../assets/Logo/LogoWhite.png";
-import "./Navbar.css";
-
-class Navbar extends Component {
-  componentDidMount() {
-    let navToggle = document.querySelector(".nav__toggle");
-    let navWrapper = document.querySelector(".nav__wrapper");
-
-    navToggle.addEventListener("click", function () {
-      if (navWrapper.classList.contains("active")) {
-        this.setAttribute("aria-expanded", "false");
-        this.setAttribute("aria-label", "menu");
-        navWrapper.classList.remove("active");
-      } else {
-        navWrapper.classList.add("active");
-        this.setAttribute("aria-label", "close menu");
-        this.setAttribute("aria-expanded", "true");
-      }
-    });
-  }
-  render() {
-    return (
-      <header class="site-header">
-        <div class="wrapper site-header__wrapper">
-          <a href="#" class="brand">
-            <img src={Logo} alt="okidz Logo" />
-          </a>
-          <nav class="nav">
-            <button class="nav__toggle" aria-expanded="false" type="button">
-              menu
-            </button>
-            <ul class="nav__wrapper">
-              <li class="nav__item">
-                <a href="#">Home</a>
-              </li>
-              <li class="nav__item">
-                <a href="#">About</a>
-              </li>
-              <li class="nav__item">
-                <a href="#">Services</a>
-              </li>
-              <li class="nav__item">
-                <a href="#">Hire us</a>
-              </li>
-              <li class="nav__item">
-                <a href="#">Contact</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-    );
-  }
+import React from "react";
+import Logo from "../../assets/Logo/LogoTransparent.png";
+function Navbar() {
+  return (
+    <Navbar bg="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            src={Logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Navbar;
