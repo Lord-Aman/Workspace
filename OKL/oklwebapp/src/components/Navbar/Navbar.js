e898b12e48e91ca5e53dafdbe1d897d473bbc8f3
Aman <aakashaman1770@gmail.com>
2021-11-20 17:51:20 +0000

    header Update

--- OKL/oklwebapp/src/components/Navbar/Navbar.js
+++ OKL/oklwebapp/src/components/Navbar/Navbar.js
@@ -1,20 +1,60 @@
 import React from "react";
 import Logo from "../../assets/Logo/LogoTransparent.png";
+import "./Navbar.css";
+
 function Navbar() {
   return (
-    <Navbar bg="dark">
-      <Container>
-        <Navbar.Brand href="#home">
-          <img
-            src={Logo}
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-            alt="React Bootstrap logo"
-          />
-        </Navbar.Brand>
-      </Container>
-    </Navbar>
+    <header id="header" class="fixed-top">
+      <div class="container-fluid d-flex justify-content-between align-items-center">
+        <h1 class="logo me-auto me-lg-0">
+          <a href="index.html">Kelly</a>
+        </h1>
+        <a href="index.html" class="logo">
+          <img src="assets/img/logo.png" alt="" class="img-fluid" />
+        </a>
+
+        <nav id="navbar" class="navbar order-last order-lg-0">
+          <ul>
+            <li>
+              <a class="active" href="index.html">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="about.html">About</a>
+            </li>
+            <li>
+              <a href="resume.html">Resume</a>
+            </li>
+            <li>
+              <a href="services.html">Services</a>
+            </li>
+            <li>
+              <a href="portfolio.html">Portfolio</a>
+            </li>
+            <li>
+              <a href="contact.html">Contact</a>
+            </li>
+          </ul>
+          <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+
+        <div class="header-social-links">
+          <a href="#" class="twitter">
+            <i class="bi bi-twitter" />
+          </a>
+          <a href="#" class="facebook">
+            <i class="bi bi-facebook" />
+          </a>
+          <a href="#" class="instagram">
+            <i class="bi bi-instagram" />
+          </a>
+          <a href="#" class="linkedin">
+            <i class="bi bi-linkedin" />
+          </a>
+        </div>
+      </div>
+    </header>
   );
 }
 
--- OKL/oklwebapp/src/pages/Index.js
+++ OKL/oklwebapp/src/pages/Index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Layout from "../components/Layout";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 function index() {
   return <Layout />;
 }
