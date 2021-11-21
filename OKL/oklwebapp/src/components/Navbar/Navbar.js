39606510c84e366f5ac167d3322d8aa84ba4ab7b
Aman <aakashaman1770@gmail.com>
2021-11-21 07:27:17 +0000

    update okl

--- OKL/oklwebapp/src/components/Navbar/Navbar.js
+++ OKL/oklwebapp/src/components/Navbar/Navbar.js
@@ -1,60 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
 import Logo from "../../assets/Logo/LogoTransparent.png";
 import "./Navbar.css";
 
-function Navbar() {
-  return (
-    <header id="header" class="fixed-top">
-      <div class="container-fluid d-flex justify-content-between align-items-center">
-        <h1 class="logo me-auto me-lg-0">
-          <a href="index.html">
-            <img src={Logo} alt="Okidz Logo" />
-          </a>
-        </h1>
-        <a href="index.html" class="logo">
-          <img src="assets/img/logo.png" alt="" class="img-fluid" />
-        </a>
+class Navbar extends Component {
+  componentDidMount() {
+    const hamburger = document.querySelectorAll(".navbar .dropdown > a");
 
-        <nav id="navbar" class="navbar order-last order-lg-0">
-          <ul>
-            <li>
-              <a class="active" href="index.html">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#">About Us</a>
-            </li>
-            <li>
-              <a href="#">Gallery</a>
-            </li>
-            <li>
-              <a href="#">Creators</a>
-            </li>
-            <li>
-              <a href="#">Activities for Kids</a>
-            </li>
-          </ul>
-          <i class="bi bi-list mobile-nav-toggle"></i>
-        </nav>
+    document.querySelector(".dropdown > a").addEventListener("click", (e) => {
+      console.log("Hamburger Clicked");
+    });
+  }
 
-        <div class="header-social-links">
-          <a href="#" class="twitter">
-            <i class="bi bi-twitter" />
+  render() {
+    return (
+      <header id="header" class="fixed-top">
+        <div class="container-fluid d-flex justify-content-between align-items-center">
+          <h1 class="logo me-auto me-lg-0">
+            <a href="index.html">
+              <img src={Logo} alt="Okidz Logo" />
+            </a>
+          </h1>
+          <a href="index.html" class="logo">
+            <img src="assets/img/logo.png" alt="" class="img-fluid" />
           </a>
-          <a href="#" class="facebook">
-            <i class="bi bi-facebook" />
-          </a>
-          <a href="#" class="instagram">
-            <i class="bi bi-instagram" />
-          </a>
-          <a href="#" class="linkedin">
-            <i class="bi bi-linkedin" />
-          </a>
+
+          <nav id="navbar" class="navbar order-last order-lg-0">
+            <ul>
+              <li>
+                <a class="active" href="index.html">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#">About Us</a>
+              </li>
+              <li>
+                <a href="#">Gallery</a>
+              </li>
+              <li>
+                <a href="#">Creators</a>
+              </li>
+              <li>
+                <a href="#">Activities for Kids</a>
+              </li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+          </nav>
+
+          <div class="header-social-links">
+            <a href="#" class="twitter">
+              <i class="bi bi-twitter" />
+            </a>
+            <a href="#" class="facebook">
+              <i class="bi bi-facebook" />
+            </a>
+            <a href="#" class="instagram">
+              <i class="bi bi-instagram" />
+            </a>
+            <a href="#" class="linkedin">
+              <i class="bi bi-linkedin" />
+            </a>
+          </div>
         </div>
-      </div>
-    </header>
-  );
+      </header>
+    );
+  }
 }
 
 export default Navbar;
