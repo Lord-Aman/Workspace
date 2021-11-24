41668e140e93c62fb8031987447de096b917437d
Aman <aakashaman1770@gmail.com>
2021-11-24 11:15:14 +0000

    Updated Commit - OKL

--- OKL/oklwebapp/src/components/Slider/carousel/corousel.jsx
+++ OKL/oklwebapp/src/components/Slider/carousel/corousel.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import Banner1 from "../banner1";
 import Banner2 from "../banner2";
+import Banner3 from "../banner3";
 import Banner4 from "../banner4";
 
 import "./carousel.css";
