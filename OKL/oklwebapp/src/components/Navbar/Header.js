bc971e9d892ba9da0d9e44c20a21bca50d7e4235
Aman <aakashaman1770@gmail.com>
2021-11-26 07:35:50 +0000

    Updated Commit

--- OKL/oklwebapp/src/components/Navbar/Header.js
+++ OKL/oklwebapp/src/components/Navbar/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Logo from "../../assets/Logo/LogoWhite.svg";
 
 function Header() {
+
     return (
         <div style={styles.container}>
             <div class="logo" style={styles.logo}><img src={Logo} style={styles.logoImage} alt="LogoImage" /><h1 style={styles.logoHeader}>OKidzLabs</h1></div>
