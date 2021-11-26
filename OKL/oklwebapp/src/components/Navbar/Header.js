3cf3cfa9c5c7cf71c21082f910ed988421aae7a3
Aman <aakashaman1770@gmail.com>
2021-11-26 06:54:03 +0000

    Updated Commit

--- OKL/oklwebapp/src/components/Navbar/Header.js
+++ OKL/oklwebapp/src/components/Navbar/Header.js
@@ -1,19 +1,39 @@
 import React from 'react'
+import Logo from "../../assets/Logo/LogoTransparent.png";
 
 function Header() {
     return (
-        <div styles={styles.Container}>
-            <div class="logo" ></div>
-            <div class="navContent"></div>
-            <div class="social"></div>
+        <div style={styles.container}>
+            <div class="logo" style={styles.logo}><img src={Logo} style={styles.logoImage} alt="LogoImage" /></div>
+            <div class="navContent" style={styles.navContent}></div>
+            <div class="social" style={styles.social}></div>
         </div>
     )
 }
 
 const styles = {
-    Container : {
+    container : {
+        display:"flex",
         height: "60px",
-        backgroundColor:"red"
+        // backgroundColor:"red",
+        justifyContent:"space-evenly",
+        borderBottom:"1px solid tomato",
+        boxShadow:"0px 0px 4px 4px #f5aa42"
+    },
+    logo:{
+        // backgroundColor:"yellow",
+        width: "20%"
+    },
+    logoImage:{
+        fill:"tomato",
+    },
+    navContent:{
+        // backgroundColor:"green",
+        width:"60%",
+    },
+    social:{
+        // backgroundColor:"cyan",
+        width: "20%"
     }
 }
 
