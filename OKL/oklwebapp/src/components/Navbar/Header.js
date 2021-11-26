e5f960851965ed550f9df8e814f6aaee9c4024cd
Aman <aakashaman1770@gmail.com>
2021-11-26 09:32:27 +0000

    Updated commit

--- OKL/oklwebapp/src/components/Navbar/Header.js
+++ OKL/oklwebapp/src/components/Navbar/Header.js
@@ -1,98 +1,41 @@
 import React from "react";
 import Logo from "../../assets/Logo/LogoWhite.svg";
-
+import "./Header.css"
 function Header() {
   return (
-    <div style={styles.container}>
-      <div class="logo" style={styles.logo}>
-        <img src={Logo} style={styles.logoImage} alt="LogoImage" />
-        <h1 style={styles.logoHeader}>OKidzLabs</h1>
+    <div className="navContainer">
+      <div className="logo">
+        <img src={Logo} className="logoImage" alt="LogoImage" />
+        <h1  className="logoHeader">OKidzLabs</h1>
       </div>
-      <div class="navContent" style={styles.navContent}>
-        <ul style={styles.navList}>
+      <div className="navContent">
+        <ul className="navList">
           <li>
-            <a style={styles.navLinks} href="#">
+            <a className="navLinks" href="#">
               Home
             </a>
           </li>
           <li>
-            <a style={styles.navLinks} href="#">
+            <a className="navLinks" href="#">
               Gallery
             </a>
           </li>
           <li>
-            <a style={styles.navLinks} href="#">
+            <a className="navLinks" href="#">
               Meet the Creators
             </a>
           </li>
           <li>
-            <a style={styles.navLinks} href="#">
+            <a className="navLinks" href="#">
               Activities for Kids
             </a>
           </li>
         </ul>
       </div>
-      <div style={styles.social}></div>
+      <div className="social"></div>
     </div>
   );
 }
 
-const styles = {
-  container: {
-    display: "flex",
-    height: "60px",
-    // backgroundColor:"red",
-    justifyContent: "space-evenly",
-    borderBottom: "1px solid tomato",
-    boxShadow: "0px 0px 4px 4px #f5aa42",
-  },
-  logo: {
-    // backgroundColor:"yellow",
-    display: "flex",
-    color: "tomato",
-    alignItems: "center",
-    fontFamily: "cursive",
-    width: "250px",
-    marginLeft: "10px",
-  },
-  logoImage: {
-    height: "58px",
-    color: "orange",
-    fill: "orange",
-  },
-  logoHeader: {
-    fontSize: "26px",
-    fontFamily: "cursive",
-  },
-  navContent: {
-    // backgroundColor:"green",
-    width: "60%",
-  },
-  navList: {
-    display: "flex",
-    listStyle: "none",
-    justifyContent: "space-evenly",
-    marginTop: "16px",
-  },
-  navLinks: {
-    display: "block",
-    textDecoration: "none",
-    color: "orange",
-    fontSize: "18px",
-    fontWeight: "500",
-  },
-  social: {
-    backgroundColor: "cyan",
-    width: "20%",
-  },
-
-  // Media Queries
-  "@media(minWidth:991px)": {
-    navList: {
-      display: "none",
-      color: "#fff",
-    },
-  },
-};
 
 export default Header;
