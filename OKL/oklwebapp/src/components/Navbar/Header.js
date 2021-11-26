8686e51bff4a7e02004e8faa030d9b084e6c6c5c
Aman <aakashaman1770@gmail.com>
2021-11-26 07:15:45 +0000

    Updated Commit

--- OKL/oklwebapp/src/components/Navbar/Header.js
+++ OKL/oklwebapp/src/components/Navbar/Header.js
@@ -4,8 +4,15 @@ import Logo from "../../assets/Logo/LogoWhite.svg";
 function Header() {
     return (
         <div style={styles.container}>
-            <div class="logo" style={styles.logo}><img src={Logo} style={styles.logoImage} alt="LogoImage" /><h1>OKidzLabs</h1></div>
-            <div class="navContent" style={styles.navContent}></div>
+            <div class="logo" style={styles.logo}><img src={Logo} style={styles.logoImage} alt="LogoImage" /><h1 style={styles.logoHeader}>OKidzLabs</h1></div>
+            <div class="navContent" style={styles.navContent}>
+                <ul style={styles.navList}>
+                    <li><a style={styles.navLinks} href="#">Home</a></li>
+                    <li><a style={styles.navLinks} href="#">Home</a></li>
+                    <li><a style={styles.navLinks} href="#">Home</a></li>
+                    <li><a style={styles.navLinks} href="#">Home</a></li>
+                </ul>
+            </div>
             <div class="social" style={styles.social}></div>
         </div>
     )
@@ -26,16 +33,27 @@ const styles = {
         color:"tomato",
         alignItems:"orange",
         fontFamily:"cursive",
-        width: "20%"
+        width: "250px",
+        marginLeft:"10px",
     },
     logoImage:{
         height:"58px",
         color:"orange",
         fill : "orange"
     },
+    logoHeader:{
+        fontSize:"26px",
+        fontFamily:"cursive"
+    },
     navContent:{
         // backgroundColor:"green",
         width:"60%",
+    },
+    navList:{
+        display:"flex",
+        listStyle:"none",
+        justifyContent:"space-evenly",
+        alignItems:"center"
     },
     social:{
         // backgroundColor:"cyan",
