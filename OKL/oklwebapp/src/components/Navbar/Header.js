5434d5aa29736f2f08ef3330d8c2ab5361a6b8e8
Aman <aakashaman1770@gmail.com>
2021-11-26 06:24:40 +0000

    Updated Commit

---
+++ OKL/oklwebapp/src/components/Navbar/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <div class="logo" ></div>
+            <div class="navContent"></div>
+            <div class="social"></div>
+        </div>
+    )
+}
+
+export default Header
