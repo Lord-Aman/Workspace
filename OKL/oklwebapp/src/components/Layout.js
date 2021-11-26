8b58f8077fde00db3d35c6dc61b453639fd1b019
Aman <aakashaman1770@gmail.com>
2021-11-26 06:28:24 +0000

    Updated Commit

--- OKL/oklwebapp/src/components/Layout.js
+++ OKL/oklwebapp/src/components/Layout.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Header from "./Navbar/Header";
 import Navbar from "./Navbar/Navbar";
 import Corousel from "./Slider/carousel/corousel";
 
 function Layout() {
   return (
     <>
-      <Navbar />
-      <Corousel />
+      <Header/>
+      {/* <Navbar />
+      <Corousel /> */}
     </>
   );
 }
--- OKL/oklwebapp/src/components/Navbar/Header.js
+++ OKL/oklwebapp/src/components/Navbar/Header.js
@@ -2,12 +2,19 @@ import React from 'react'
 
 function Header() {
     return (
-        <div>
+        <div styles={styles.Container}>
             <div class="logo" ></div>
             <div class="navContent"></div>
             <div class="social"></div>
         </div>
     )
+}
+
+const styles = {
+    Container : {
+        height: "60px",
+        backgroundColor:"red"
+    }
 }
 
 export default Header
