30cd6d4746bd0f7b846187a620d45b7afe5928af
Aman <aakashaman1770@gmail.com>
2021-11-29 06:19:44 +0000

    Updated Commit

--- OKL/oklwebapp/src/components/Layout.js
+++ OKL/oklwebapp/src/components/Layout.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Navbar/Header";
+import Banner from "./Slider/Banner";
 import Corousel from "./Slider/carousel/corousel";
 
 function Layout() {
   return (
     <>
       <Header />
-      <Corousel />
+      {/* <Corousel /> */}
+      <Banner />
     </>
   );
 }
--- OKL/oklwebapp/src/components/Slider/Banner.js
+++ OKL/oklwebapp/src/components/Slider/Banner.js
@@ -2,8 +2,23 @@ import React from "react";
 import "./Banner.css";
 function Banner() {
   return (
-    <div>
-      <h1>Banner</h1>
+    <div className="sliderContainer">
+      <div className="sliderText">
+        <h1>
+          Giving your kids the best future, Now give them the best past too{" "}
+        </h1>
+        <p>
+          Get your blood tests delivered at let home collect sample from the
+          victory of the managements that supplies best design system guidelines
+          ever
+        </p>
+      </div>
+      <div className="sliderImage">
+        <img
+          src="https://techmaster.netlify.app/static/banner1-96199963430a92bf5c15349943ec90aa.gif"
+          alt="gif"
+        />
+      </div>
     </div>
   );
 }
