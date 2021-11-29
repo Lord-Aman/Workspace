0fce45ee14a9ea55e769fd12e96c04475b032b5e
Aman <aakashaman1770@gmail.com>
2021-11-29 07:12:26 +0000

    Updated Commit

--- OKL/oklwebapp/src/components/Activities/Activities.js
+++ OKL/oklwebapp/src/components/Activities/Activities.js
@@ -18,7 +18,21 @@ function Activities() {
           <button>Explore</button>
         </div>
       </div>
-      <div className="activity2"></div>
+      <div className="activity2">
+        <div className="boxContainer">
+          <img
+            src="https://techmaster.netlify.app/static/f-icon3-0c208c1f807c7e75455d316b55c27c2f.svg"
+            alt="img"
+          />
+          <h1>Heading</h1>
+          <p>
+            Your Website Should Look Good On Any Platform. The Users Of
+            Smartphone Are Increasing Exponentially, We Help Improve Your
+            Visitor’s Experience.
+          </p>
+          <button>Explore</button>
+        </div>
+      </div>
     </div>
   );
 }
