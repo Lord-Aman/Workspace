22040809a05a47c8ffe39b2b4db7b1ffd822a910
Aman <aakashaman1770@gmail.com>
2021-11-29 06:28:48 +0000

    Updated Commit

--- OKL/oklwebapp/src/components/Layout.js
+++ OKL/oklwebapp/src/components/Layout.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Header from "./Navbar/Header";
-import Banner from "./Slider/Banner1";
 import Corousel from "./Slider/carousel/corousel";
 
 function Layout() {
   return (
     <>
       <Header />
-      {/* <Corousel /> */}
-      <Banner />
+      <Corousel />
     </>
   );
 }
